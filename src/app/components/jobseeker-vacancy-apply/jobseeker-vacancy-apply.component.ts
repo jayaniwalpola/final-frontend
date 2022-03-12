ce19f77461a8a1b1f4d9bf7ebdeea53fadbf3905
Jayani  Walpola <jayaniwalpola1234@gmail.com>
2022-03-12 18:09:30 +0530

    vacancy apply data retrive

--- src/app/components/jobseeker-vacancy-apply/jobseeker-vacancy-apply.component.ts
+++ src/app/components/jobseeker-vacancy-apply/jobseeker-vacancy-apply.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/service/data.service';
+import { Vacancy } from 'src/app/vacancy';
 
 @Component({
   selector: 'app-jobseeker-vacancy-apply',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobseekerVacancyApplyComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  data: any;
+  vacancy = new Vacancy();
+
+  constructor(private route:ActivatedRoute,private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.getData();
+    // console.log(this.route.snapshot.params.id);
+
   }
+  getData(){
+    this.dataService.vacancyGetById(this.id).subscribe(res => {
+      // console.log(res);
+      this.data = res;
+      this.vacancy = this.data;
+    })
+  }
+
 
 }
